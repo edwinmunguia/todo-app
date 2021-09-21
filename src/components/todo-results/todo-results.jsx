34d8f76a706322f756859454549e99f241c8eac5
edwinmunguia <edwinj.munguia90@gmail.com>
2021-09-21 10:18:52 -0600

    Count completed tasks - Implement reducer to get completed tasks count

--- src/components/todo-results/todo-results.jsx
+++ src/components/todo-results/todo-results.jsx
@@ -1,9 +1,13 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
 import './todo-results.scss';
+import { TodosContext } from "../../todo-context";
 
 export const TodoResults = () => {
+  const { todos } = useContext(TodosContext);
+
   const calculateChecked = () => {
     // Fix an ability to calculate completed tasks
+    return todos.reduce((acc, todo) => todo.checked ? acc + 1 : acc, 0);
   };
 
   return (
